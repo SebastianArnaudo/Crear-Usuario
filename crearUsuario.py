a8fc8e6690344350fb69395817a8c6565704d9ec
Sebastian Arnaudo <sebastianarnaudo.lamaruja@gmail.com>
2022-12-01 16:31:05 -0300

    creandoBaseDatos y otros campos

--- crearUsuario.py
+++ crearUsuario.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import baseDatos 
 
 ventana = tk.Tk()
 ventana.title("ADM Usuarios")
@@ -20,29 +21,35 @@ etiquetaApellido.place(x= 30, y= 100)
 entradaApellido = tk.Entry()
 entradaApellido.place(x= 150, y= 100)
 
+etiquetaUsuario = tk.Label(text= "Usuario:")
+etiquetaUsuario.place(x= 30, y= 150)
+
+entradaNombre = tk.Entry()
+entradaNombre.place(x= 150, y= 150)
+
 etiquetaNacimiento = tk.Label(text= "Fecha Nacimiento:")
-etiquetaNacimiento.place(x= 30, y= 150)
+etiquetaNacimiento.place(x= 30, y= 200)
 
 entradaNacimiento = tk.Entry()
-entradaNacimiento.place(x= 150, y= 150)
+entradaNacimiento.place(x= 150, y= 200)
 
 
 etiquetaCorreo = tk.Label(text= "Correo Electronico:")
-etiquetaCorreo.place(x= 30, y= 200)
+etiquetaCorreo.place(x= 30, y= 250)
 
 entradaCorreo = tk.Entry()
-entradaCorreo.place(x= 150, y= 200)
+entradaCorreo.place(x= 150, y= 250)
 
 
-etiquetaContrasenia = tk.Label(text= "contraseña:")
-etiquetaContrasenia.place(x= 30, y= 250)
+etiquetaContrasenia = tk.Label(text= "Contraseña:")
+etiquetaContrasenia.place(x= 30, y= 300)
 
 entradaContrasenia = tk.Entry()
-entradaContrasenia.place(x= 150, y= 250)
+entradaContrasenia.place(x= 150, y= 300)
 
 
 boton = tk.Button(text= "Guardar")
-boton.place(x= 160, y= 300)
+boton.place(x= 160, y= 350)
 
 
 tk.mainloop()
